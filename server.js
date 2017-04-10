6a90344b980d7655b77c8e9edd8c7f88cc73fde1
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-10 20:16:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/project/:project_id', function (req, res){
 
 function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ["pbkdf2", "10000", salt, hashed.toString('hex')];
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join("$");
 }
 
 app.get('/hash/:input', function(req, res) {
