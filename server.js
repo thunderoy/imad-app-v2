dd488815c50ffb2780337afde6145061ce2e6270
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-10 19:39:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/project/:project_id', function (req, res){
                 res.status(404).send("Project not found");
             }  else {
                   var projectData = result.rows[0];
-                  res.send(JSON.stringify(result));
+                  res.send(JSON.stringify(projectData));
             }
         }
     });
