7942a17ed188fad6a0815632b78be24eabcfa8e8
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-10 20:08:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'thunderoy',
@@ -33,6 +34,16 @@ app.get('/project/:project_id', function (req, res){
             }
         }
     });
+});
+
+function hash (input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash (req.params.input, 'this_is_some_string');
+    res.send(hashedString);
 });
 
 app.get('/ui/style.css', function (req, res) {
