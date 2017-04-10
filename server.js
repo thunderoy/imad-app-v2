195bc4c44f8333a0bb1e59fb12575abb896cf6f3
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-10 21:00:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'thunderoy',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -44,6 +46,20 @@ function hash (input, salt) {
 app.get('/hash/:input', function(req, res) {
     var hashedString = hash (req.params.input, 'this_is_some_string');
     res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result){
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User successfully created: ' + username);
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
