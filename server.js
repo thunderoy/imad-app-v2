ecd3636612fd3d6b35fa495856a80c32a223072c
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-10 21:33:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/hash/:input', function(req, res) {
     res.send(hashedString);
 });
 
-app.post('/create-user', function(req, res){
+app.post('/register', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
@@ -59,6 +59,30 @@ app.post('/create-user', function(req, res){
         } else {
             res.send('User successfully created: ' + username);
         }
+    });
+});
+
+app.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result){
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(403).send('username/password is invalid');
+            }  else {
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password, salt);
+                if (hashedPassword === dbString) {
+                    res.send('Succesfully logged in');
+                }  else {
+                    res.status(403).send('username/password is invalid');
+                }
+        }
+      }
     });
 });
 
