368dba59c31d136fbe5dbaaabb85550f923c93fa
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-15 05:08:12 +0530

    [imad-console] Updates ui/project.js

--- ui/project.js
+++ ui/project.js
@@ -77,9 +77,7 @@ function loadComments () {
                     var time = new Date(commentsData[i].timestamp);
                     content += `<div class="comment">
                         <p>${escapeHTML(commentsData[i].comment)}</p>
-                        <div class="commenter">
-                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
-                        </div>
+                        
                     </div>`;
                 }
                 comments.innerHTML = content;
