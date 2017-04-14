2941885c549e30146d5256f2d80ca7156c2943d5
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-15 03:32:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,29 +7,9 @@ window.onclick = function(event) {
     }
 };
 
-// var loginArea = document.getElementById('login_area');
-
-// loginArea.onclick = function () {
-//     if (loginArea.innerHTML ==  "Login/Register") {
-//         document.getElementById('id01').style.display='block';
-//     }
-    
-//     else {
-//         loginArea.innerHTML = ;
-//     }
-// };
 
 
 function loadLoginForm () {
-    // var loginHtml = `
-    //     <h3>Login/Register to unlock awesome features</h3>
-    //     <input type="text" id="username" placeholder="username" />
-    //     <input type="password" id="password" />
-    //     <br/><br/>
-    //     <input type="submit" id="login_btn" value="Login" />
-    //     <input type="submit" id="register_btn" value="Register" />
-    //     `;
-    // document.getElementById('login_area').innerHTML = loginHtml;
     
     // Submit username/password to login
     var submit = document.getElementById('login_btn');
@@ -42,15 +22,15 @@ function loadLoginForm () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
+                  submit.innerHTML = 'Sucess!';
               } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
+                  submit.innerHTML = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
-                  submit.value = 'Login';
+                  submit.innerHTML = 'Login';
               } else {
                   alert('Something went wrong on the server');
-                  submit.value = 'Login';
+                  submit.innerHTML = 'Login';
               }
               loadLogin();
           }  
@@ -60,12 +40,12 @@ function loadLoginForm () {
         // Make the request
         var username = document.getElementById('username_login').value;
         var password = document.getElementById('password_login').value;
-        // console.log(username);
-        // console.log(password);
+        console.log(username);
+        console.log(password);
         request.open('POST', '/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logging in...';
+        submit.innerHTML = 'Logging in...';
         
     };
     
@@ -80,10 +60,10 @@ function loadLoginForm () {
               // Take some action
               if (request.status === 200) {
                   alert('User created successfully');
-                  register.value = 'Registered!';
+                  register.innerHTML = 'Registered!';
               } else {
                   alert('Could not register the user');
-                  register.value = 'Register';
+                  register.innerHTML = 'Register';
               }
           }
         };
@@ -100,7 +80,7 @@ function loadLoginForm () {
         request.open('POST', '/register', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
-        register.text = 'Registering...';
+        register.innerHTML = 'Registering...';
     
     };
 }
