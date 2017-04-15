2e4673946f8d9ea321ffc14ed2834e97332df611
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-04-15 12:07:12 +0530

    [imad-console] Updates ui/project.js

--- ui/project.js
+++ ui/project.js
@@ -1,6 +1,6 @@
 var currentProjectId = window.location.pathname.split('/')[2];
 
-window.onload = function loadCommentForm () {
+function loadCommentForm () {
     var commentFormHtml = `
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
@@ -64,7 +64,7 @@ function escapeHTML (text)
     return $div.innerHTML;
 }
 
-window.onload = function loadComments () {
+function loadComments () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
